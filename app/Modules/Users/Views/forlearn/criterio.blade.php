4bf0bc44220833d5565677d1e61a40616765d1eb
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-06-02 12:58:30 +0100

    refactor: aumentando o sistema de seguranca

--- app/Modules/Users/Views/forlearn/criterio.blade.php
+++ app/Modules/Users/Views/forlearn/criterio.blade.php
@@ -20,13 +20,14 @@
         <h1>forlearn</h1>
         <h4>Este é uma actualização da forlearn. </h4>
         <p>porfavor atulize o seu Whatsapp pará poder utilizar os novos serviços</p>
-        <form action="{{ route('actualizar-whatsapp') }}" method="POST" class="form-control">
+        <form action="{{ route('actualizar-whatsapp') }}" method="POST" class="form-control" id="formularioLogin">
             <div class="mb-3 ">
+                <div id="error" class="text-danger"></div>
                 <label for="criterio" class="form-label">
                     <i class="bi bi-whatsapp"></i>
                     <strong>Whatsapp</strong>
                 </label>
-                <input type="text" class="form-control" id="criterio" name="criterio" placeholder="ex: 945347861" required>
+                <input type="text" class="form-control" id="numero" name="criterio" placeholder="ex: 945347861" required>
                 <input type="hidden" name="id" value="{{ $id }}">
             </div>
             <div class="d-flex gap-3">
@@ -35,6 +36,62 @@
             </div>
         </form>
     </div>
+    <script>
+      const inputNumero = document.getElementById('numero');
+      const error = document.getElementById('error');
+      const formulario = document.getElementById('formularioLogin')
+
+      function validarNumero(numero){
+        if(!/^\d{0,9}$/.test(numero)){
+          return 'O numero deve conter apenas 9 digitos'
+        }
+        if(numero.length < 9){
+          return ""; // ainda a digitar
+        }
+        if(numero[0] !== '9'){
+          return 'O numero deve comecar com 9'
+        }
+        if(numero[1] === '9' && numero[2] === '9' ){
+          return 'O numero 999 nao é permitido!'
+        }
+        if (numero.startsWith('9')) {
+          const restantes = numero.slice(1); // pega os 8e últimos dígitos
+          const primeiroRepetido = restantes[0];
+
+          if ([...restantes].every(d => d === primeiroRepetido)) {
+            return 'Números inventados não são permitido.';
+          }
+        }
+         // Bloqueio de repetições tipo 990000000, 998888888
+        if (numero.startsWith('99')) {
+          const restantes = numero.slice(2); // pega os 7 últimos dígitos
+          const primeiroRepetido = restantes[0];
+
+          if ([...restantes].every(d => d === primeiroRepetido)) {
+            return 'Números inventados (repetitivos) não são permitidos após 99.';
+          }
+        }
+        return '' //tudo certo
+      }
+
+       inputNumero.addEventListener('input', function () {
+        const numero = inputNumero.value.trim();
+        const mensagemErro = validarNumero(numero);
+        error.textContent = mensagemErro;
+      });
+
+
+      formulario.addEventListener('submit', function (event) {
+      const numero = inputNumero.value.trim();
+      const mensagemErro = validarNumero(numero);
+
+      if (mensagemErro !== '') {
+        error.textContent = mensagemErro;
+        event.preventDefault();
+      }
+    });
+      
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
