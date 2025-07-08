fef7caa33b0fcd95878a38b432a7bfb0c131cba2
R0CKST4R-02 <ezequiel.p.a.francisco@gmail.com>
2025-07-08 14:41:59 +0100

    feat/tabela estatitca

---
+++ app/Modules/Users/Views/estatisticaget/index.blade.php
@@ -0,0 +1,280 @@
+<title> Estatistica de pagamento | forLEARN® by GQS</title>
+@extends('layouts.generic_index_new')
+@section('page-title', 'Estatistica de pagamento')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="/">Home</a></li>
+    <li class="breadcrumb-item"><a href="{{ route('panel_avaliation') }}">****</a></li>
+    <li class="breadcrumb-item active" aria-current="page">*******</li>
+@endsection
+
+@section('styles-new')
+    @parent
+    <link rel="stylesheet" href="{{ asset('css/new_table_panel.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/new_switcher.css') }}">
+@endsection
+
+@section('selects')
+    <div class="mb-2">
+        <label for="lective_year">Ano lectivo</label>
+        <select name="lective_year" id="lective_year" class="form-control form-control-sm">
+        @foreach ($lectiveYears as $lectiveYear)
+            <option value="{{ $lectiveYear->id }}" @if ($lectiveYearSelected == $lectiveYear->id) selected @endif>
+                {{ $lectiveYear->currentTranslation->display_name }}
+            </option>
+        @endforeach
+        </select>
+    </div>
+@endsection
+
+@section('body')
+
+    <div class="row">
+        <div class="d-flex justify-content-between w-100">
+            <!-- Curso -->
+            <div class="d-flex flex-column me-3" style="flex: 1;">
+                <label for="selectorCurso">Curso</label>
+                <select class="form-control form-control-sm" name="curso" id="selectorCurso">
+                    <option selected>Selecione o Curso</option>
+                    {{--@foreach ($courses as $c)
+                        <option value="{{ $c->id }}">{{ $c->currentTranslation->display_name }}</option>
+                    @endforeach--}}
+                </select>
+            </div>
+
+            <!-- Turma -->
+            <div class="d-flex flex-column ms-3" style="flex: 1;">
+                <label for="selectorTurma">Turma</label>
+                <select class="form-control form-control-sm" name="turma" id="selectorTurma">
+                    <option selected>Escolha a turma</option>
+                    <!-- JS vai preencher -->
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mr-1">
+        <table>
+            <thead>
+                <tr>
+                    <th rowspan="2"></th>
+                    <th colspan="4">1ano</th>
+                    <th colspan="4">2ano</th>
+                    <th colspan="4">3ano</th>
+                    <th colspan="4">4ano</th>
+                    <th colspan="4">5ano</th>
+                </tr>
+                <tr>
+                    <!-- 1ano -->
+                    <th>M</th>
+                    <th>T</th>
+                    <th>N</th>
+                    <th>PT</th>
+                    
+                    <!-- 2ano -->
+                    <th>M</th>
+                    <th>T</th>
+                    <th>N</th>
+                    <th>PT</th>
+                    
+                    <!-- 3ano -->
+                    <th>M</th>
+                    <th>T</th>
+                    <th>N</th>
+                    <th>PT</th>
+                    
+                    <!-- 4ano -->
+                    <th>M</th>
+                    <th>T</th>
+                    <th>N</th>
+                    <th>PT</th>
+                    
+                    <!-- 5ano -->
+                    <th>M</th>
+                    <th>T</th>
+                    <th>N</th>
+                    <th>PT</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>EC</td>
+                    <td>23</td>
+                    <td>54</td>
+                    <td>87</td>
+                    <td>09</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>ENT</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>HID</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>GEO</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>PET</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>QUI</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>REC</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="d-flex mb-2 gap-3">
+        <button id="pdf" class="btn btn-success">
+            <i class="fas fa-file-pdf"></i> Gerar PDF
+        </button>
+        <!--<button type="submit" class="btn btn-dark">
+            <i class="fas fa-file-pdf"></i> Sem ação
+        </button>-->
+    </div>
+@endsection
+
+@section('scripts-new')
+<script>
+  
+</script>
+    
+@endsection
