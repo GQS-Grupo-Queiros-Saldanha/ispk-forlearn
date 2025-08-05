e6513d0fc4f6ff5714c51cad472027a6a95b1168
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-08-05 09:37:21 +0100

    fix/001

--- app/Modules/GA/helper.php
+++ app/Modules/GA/helper.php
@@ -211,7 +211,7 @@ function EmolumentCodevLective($search, $lective_year_id)
         ->join('code_developer as code', 'code.id', '=', 'art.id_code_dev')
         ->select(['art.code', 'art.id as id_emolumento', 'art.anoLectivo as lectiveYear', 'code.code as codigo_dev'])
         ->where('code.code', $search)
-        ->where('art.anoLectivo', $lective_year_id->id)
+        ->where('art.anoLectivo', 9) //$lective_year_id->id
         ->get();
     return $Consulta;
 }
