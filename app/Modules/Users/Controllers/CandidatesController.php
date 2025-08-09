7f97d3fcda1a7f716f0690381b5d5f6e75fc1845
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-08-09 03:06:48 +0100

    fix/listar candidadis

--- app/Modules/Users/Controllers/CandidatesController.php
+++ app/Modules/Users/Controllers/CandidatesController.php
@@ -277,9 +277,10 @@ class CandidatesController extends Controller
         ->where('uca.year_fase_id', $lectiveCandidate->id)
         ->get();
         
-      //dd($model);
+      dd($model);
 
       $cursos = $this->candidateUtil->cursoQueryGet($lectiveYear);
+      //dd($cursos);
 
 
       return Datatables::of($model)
