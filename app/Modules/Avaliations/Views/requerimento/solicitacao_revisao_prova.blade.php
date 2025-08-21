01505dd06e1ac00d905ae7355abc6737c99d4750
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-08-21 16:50:03 +0100

    fix/emolumentos no processo de matrícula

--- app/Modules/Avaliations/Views/requerimento/solicitacao_revisao_prova.blade.php
+++ app/Modules/Avaliations/Views/requerimento/solicitacao_revisao_prova.blade.php
@@ -18,74 +18,14 @@
     <div class="mb-2 mt-3">
         <label for="lective_year">Selecione o ano lectivo</label>
 
-        <select name="lective_year" id="lective_year" class="selectpicker form-control form-control-sm"
-            style="width: 100%; !important">
-            @foreach ($lectiveYears as $lectiveYear)
-                @if ($lectiveYearSelected == $lectiveYear->id)
-                    <option value="{{ $lectiveYear->id }}" selected>
-                        {{ $lectiveYear->currentTranslation->display_name }}
-                    </option>
-                @else
-                    <option value="{{ $lectiveYear->id }}">
-                        {{ $lectiveYear->currentTranslation->display_name }}
-                    </option>
-                @endif
-            @endforeach
+        <select name="lective_year" id="lective_year" class="selectpicker form-control form-control-sm">
+           
         </select>
     </div>
 @endsection
 
 @section('body')
-    {!! Form::open(['route' => ['student_tfc_store']]) !!}
-
-
-    <div class="row">
-        <div class="col">
-
-
-
-            <div class="card">
-                <div class="row">
-                    <div class="col-6">
-                        <div class="form-group col">
-                            <label>Selecionar curso</label>
-                            <select name="course_id" id="courses" class="selectpicker form-control form-control-sm">
-                                <option value="" selected></option>
-                                @foreach ($courses as $course)
-                                    <option value="{{ $course->id }}">
-                                        {{ $course->currentTranslation->display_name }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="col-6">
-                        <div class="form-group col">
-                            <label>Estudante</label>
-                            {{ Form::bsLiveSelectEmpty('students', [], null, ['id' => 'students', 'class' => 'form-control']) }}
-                        </div>
-                    </div>
-                </div>
-
-                <input type="hidden" id="lectiveY" value="" name="anoLectivo">
-
-
-            </div>
-            <hr>
-            <div class="float-right">
-                <button type="submit" class="btn btn-success mb-3">
-                    <i class="fas fa-plus-circle"></i>
-                    Requerer
-                </button>
-
-
-            </div>
-        </div>
-
-    </div>
-    {!! Form::close() !!}
-
+   
 @endsection
 @section('scripts')
     @parent
