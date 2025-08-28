90553074e47a0ff289b65a9f25ad3400906701a2
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-08-28 16:24:03 +0100

    fix/relatorio das candidaturas

--- app/Modules/Users/Controllers/CandidatesController.php
+++ app/Modules/Users/Controllers/CandidatesController.php
@@ -1590,7 +1590,7 @@ class CandidatesController extends Controller
 
         return $estatisticas;
       });
-      dd($candidatos);
+    
 
 
 
@@ -1709,6 +1709,28 @@ class CandidatesController extends Controller
       $documentoCode_documento = 5;
       $logotipo = "https://" . $_SERVER['HTTP_HOST'] . "/instituicao-arquivo/" . $institution->logotipo;
       $date_generated = date("Y/m/d");
+      
+      dd(compact(
+        'vagas',
+        'cordenador',
+        'lectiveFase',
+        'lectiveYears',
+        'institution',
+        'titulo_documento',
+        'anoLectivo_documento',
+        'documentoGerado_documento',
+        'documentoCode_documento',
+        'date_generated',
+        'twoCourse',
+        'twoCourseUsers',
+        'logotipo',
+        'candidatos',
+        'todos_candidatos',
+        'staff',
+        'datas_inscricao',
+        'emolumentos'
+    ));
+
       $pdf = PDF::loadView(
         "Users::candidate.pdf-relatorios-new",
         compact(
