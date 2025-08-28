d7b7a83a0428e68d91f563161a55c67581cef1ca
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-08-28 16:55:01 +0100

    fix/relatorio das candidaturas

--- app/Modules/Users/Controllers/CandidatesController.php
+++ app/Modules/Users/Controllers/CandidatesController.php
@@ -1086,7 +1086,8 @@ class CandidatesController extends Controller
       // return $all_emolumentos;
 
       $twoCourse = [];
-      dd($cursos->pluck('state', 'usuario_id'));
+      dd($cursos);
+      //dd($cursos->pluck('state', 'usuario_id'));
       foreach ($cursos as $item) {
         if ($item->state == 'total') {
           if (isset($twoCourse[$item->usuario_id])) {
