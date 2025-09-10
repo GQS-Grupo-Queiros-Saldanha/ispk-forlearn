1184c62e58b23f430e665f8edf9d170ce3a6ca45
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-09-10 16:58:58 +0100

    fix/500

--- app/Modules/Reports/Controllers/DeclarationController.php
+++ app/Modules/Reports/Controllers/DeclarationController.php
@@ -3791,9 +3791,7 @@ return $pdf->stream($pdf_name . '.pdf');
     }
 
 
-    private function pedido_entrada($request, $config)
-    {
-        dd($request);
+    private function pedido_entrada($req
         try {
 
             $this->gerar_codigo_documento($request->requerimento);
