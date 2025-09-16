0c5a29ba2dffa159d4431c263ffeb50956ca6804
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-09-16 16:27:13 +0100

    fix/bug na tesouraria

--- app/Modules/Payments/Controllers/ArticleRequestsController.php
+++ app/Modules/Payments/Controllers/ArticleRequestsController.php
@@ -2838,7 +2838,7 @@ class ArticleRequestsController extends Controller
             }
         } catch (Exception | Throwable $e) {
             // logError($e);
-            return Request::ajax() ? response()->json(['error'=>$e->getMessage()], 500) : abort(500)
+            return Request::ajax() ? response()->json(['error'=>$e->getMessage()], 500) : abort(500);
         }
     }
     
