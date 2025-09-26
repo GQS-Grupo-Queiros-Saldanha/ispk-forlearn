d895b52e6d6dbf602473c823895d028ec0d6af85
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-09-26 12:30:19 +0100

    style/ajustando a margem no ispk

--- app/Modules/Reports/Views/pdf_model/forLEARN_footer.blade.php
+++ app/Modules/Reports/Views/pdf_model/forLEARN_footer.blade.php
@@ -21,56 +21,76 @@
         <style>
             body{
                 margin: 0;
-                padding:0;
+                padding:0 0 40px 0;
                 color: #444;
             }
             footer{
-                position: fixed;
-                bottom: 30px; /* Posição mais baixa para aparecer */
-                width: 100%;
+                margin-top: 20px;
+            
             }
-            .tb_footer{
+            footer{
+                width: 100%;
+                position: fixed; /* mantém o footer no fundo em todas as páginas */
+                bottom: 20px;    /* sobe o footer, quanto menor for este valor, mais em baixo ele fica */
+                font-family: Calibri !important;
+                color: #f7371e !important
+
+            }.tb_footer{
                 opacity:0.8;
                 width: 1000px;
+                
             }
 
             .text-center {
+            
                 text-align: right;
+                /* padding-top:80px; */
                 padding:0;
+
             }
 
             .text-right {
                 text-align:right;
+                
             }
+            #tel{
+                /* widows:40px;  */
+                /* height:40px; */
+            }
+
+        
         </style>
     </head>
 
-    <body style="@if($requerimento->codigo_documento == 12) padding-top:30px; @endif">
-        <footer style="
-            @if($requerimento->codigo_documento == 2)
-                margin-left: -20px;
-                font-size: 11.5pt !important;
-            @elseif($requerimento->codigo_documento == 10 || $requerimento->codigo_documento == 6)
-                margin-left: 150px;
-                font-size: 13pt !important;
-            @elseif($requerimento->codigo_documento == 12)
-                margin-left: 30px;
-                font-size: 13pt !important;
-            @elseif($requerimento->codigo_documento == 11)
-                margin-left: 150px;
-                font-size: 13pt !important;
-            @else
-                margin-left: 595px;
-                font-size: 15pt !important;
-            @endif
-            font-family: Calibri !important;
-            color:#f7371e!important;
-            position: fixed;
-            bottom: 30px; /* Posição ajustada para aparecer */
-            ">
+    <body style="@if($requerimento->codigo_documento == 12)
+    padding-top:30px;
+    @endif">
+        <!--<footer style="@if($requerimento->codigo_documento != 2)margin-left:595px;@else margin-left:0px @endif font-size:22pt !important;font-family:calibri light !important;">-->
+<footer        style="
+@if($requerimento->codigo_documento == 2)
+    margin-left: -20px;
+    font-size: 11.5pt !important;
+@elseif($requerimento->codigo_documento == 10 || $requerimento->codigo_documento == 6)
+    margin-left: 150px;
+    font-size: 13pt !important;
+@elseif($requerimento->codigo_documento == 12)
+    margin-left: 30px;
+    font-size: 13pt !important;
+@elseif($requerimento->codigo_documento == 11)
+    margin-left: 150px;
+    font-size: 13pt !important;
+@else
+    margin-left: 595px;
+    font-size: 15pt !important;
+@endif
+    font-family: Calibri !important;
+    color:#f7371e!important;>
+    ">
 
             <table class="tb_footer">
-                <style>#decima td{font-family:calibri light;@if($requerimento->codigo_documento == 2 || $requerimento->codigo_documento == 10)padding-left:135px; @else padding-left:0px;@endif}</style>
+                                    
+                <style>#decima td{font-family:calibri light;@if($requerimento->codigo_documento == 2 || $requerimento->codigo_documento == 10)padding-left:135px; @else padding-left:0px;@endif  }.iconeIMAg{ height:18px;position:absolute;top:20px;
+               }</style>
                 <tr id="decima">
                 
                 @if($requerimento->codigo_documento == 2)
@@ -99,38 +119,45 @@
                          {{$institution->dominio_internet}}
                     </td>
                  @elseif($requerimento->codigo_documento == 10)
-                 <td>
+                 
                          {{$institution->morada}}, {{$institution->provincia}}, Angola
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          {{$institution->dominio_internet}}
-                 </td>
                  @elseif($requerimento->codigo_documento == 12)
-                 <td>
+                 
                          {{$institution->morada}}, {{$institution->provincia}}, Angola
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          {{$institution->dominio_internet}}
-                 </td>
                  @elseif($requerimento->codigo_documento == 6)
-                 <td>
+                 
                          {{$institution->morada}}, {{$institution->provincia}}, Angola
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                          {{$institution->dominio_internet}}
-                 </td>
                 @else
                 <td> 
                 Documento nº {{$requerimento->code ?? 'código doc'}} liquidado com CP nº{{$recibo ?? 'recibo'}} 
                 </td>
+                    
+                   
                 @endif
+                   {{-- <td  class="text-right" id="tel">  <img class="iconeIMAg" src="https://img.icons8.com/ios-glyphs/50/000000/filled-message.png" />
+                        {{$institution->email}} | <img class="iconeIMAg" src="https://img.icons8.com/ios-glyphs/60/000000/phone-office--v1.png"/>{{$institution->telefone_geral}} 
+                    </td> --}}      
                 </tr>
+                {{-- <tr>
+                    <td style="" colspan="1"></td></label>
+                    <td style="color:#444; text-align:center;font-size:11pt;" >powered by forLEARN<sup>®</sup> </td> 
+                    <td style="" colspan="1"></td>
+                </tr>  --}}     
             </table>
         </footer>
     </body>
