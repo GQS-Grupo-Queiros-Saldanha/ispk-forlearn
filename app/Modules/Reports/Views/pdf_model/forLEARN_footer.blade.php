94e3d28978460bab204ee6e82c3efcfc2ec9d4ed
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-10-01 12:52:04 +0100

    style/ajustando a margem no ispk

--- app/Modules/Reports/Views/pdf_model/forLEARN_footer.blade.php
+++ app/Modules/Reports/Views/pdf_model/forLEARN_footer.blade.php
@@ -21,44 +21,46 @@
         <style>
             body{
                 margin: 0;
-                padding:0 0 40px 0;
+                padding: 0 0 20px 0; /* Reduzi o padding inferior */
                 color: #444;
+                position: relative;
+                min-height: 100vh; /* Garante que o body tenha altura mínima */
             }
             footer{
-                margin-top: 50px;
-            
-            }
-            footer{
+                margin-top: 30px; /* Reduzi a margem superior */
                 width: 100%;
-                position: absolute; /* mantém o footer no fundo em todas as páginas */
-                bottom: 5px;    /* sobe o footer, quanto menor for este valor, mais em baixo ele fica */
-                padding-top: 10px;  /* sobe os dizeres dentro do footer */
+                position: fixed; /* Mudei para fixed para melhor controle */
+                bottom: 10px; /* Aumentei um pouco para evitar corte */
+                padding-top: 5px; /* Reduzi o padding superior */
                 font-family: Calibri !important;
                 color: #f7371e !important;
-
-            }.tb_footer{
+                left: 0;
+                z-index: 100;
+            }
+            
+            .tb_footer{
                 opacity:0.8;
                 width: 1000px;
-                
             }
 
             .text-center {
-            
                 text-align: right;
-                /* padding-top:80px; */
                 padding:0;
-
             }
 
             .text-right {
                 text-align:right;
-                
             }
+            
             #tel{
                 /* widows:40px;  */
                 /* height:40px; */
             }
 
+            /* Adicionei uma classe para o conteúdo principal */
+            .main-content {
+                padding-bottom: 80px; /* Espaço para o footer não sobrepor conteúdo */
+            }
         
         </style>
     </head>
@@ -66,34 +68,51 @@
     <body style="@if($requerimento->codigo_documento == 12)
     padding-top:30px;
     @endif">
-        <!--<footer style="@if($requerimento->codigo_documento != 2)margin-left:595px;@else margin-left:0px @endif font-size:22pt !important;font-family:calibri light !important;">-->
-<footer        style="
-@if($requerimento->codigo_documento == 2)
-    margin-left: -20px;
-    font-size: 11.5pt !important;
-@elseif($requerimento->codigo_documento == 10 || $requerimento->codigo_documento == 6)
-    margin-left: 150px;
-    font-size: 13pt !important;
-@elseif($requerimento->codigo_documento == 12)
-    margin-left: 30px;
-    font-size: 13pt !important;
-@elseif($requerimento->codigo_documento == 11)
-    margin-left: 150px;
-    font-size: 13pt !important;
-@else
-    margin-left: 595px;
-    font-size: 15pt !important;
-@endif
-    font-family: Calibri !important;
-    color:#f7371e!important;>
-    ">
+        <!-- Conteúdo principal -->
+        <div class="main-content">
+            <!-- Seu conteúdo aqui -->
+        </div>
 
-            <table class="tb_footer">
-                                    
-                <style>#decima td{font-family:calibri light;@if($requerimento->codigo_documento == 2 || $requerimento->codigo_documento == 10)padding-left:135px; @else padding-left:0px;@endif  }.iconeIMAg{ height:18px;position:absolute;top:20px;
-               }</style>
-                <tr id="decima">
+        <!-- Footer -->
+        <footer style="
+            @if($requerimento->codigo_documento == 2)
+                margin-left: -20px;
+                font-size: 11.5pt !important;
+            @elseif($requerimento->codigo_documento == 10 || $requerimento->codigo_documento == 6)
+                margin-left: 150px;
+                font-size: 13pt !important;
+            @elseif($requerimento->codigo_documento == 12)
+                margin-left: 30px;
+                font-size: 13pt !important;
+            @elseif($requerimento->codigo_documento == 11)
+                margin-left: 150px;
+                font-size: 13pt !important;
+            @else
+                margin-left: 595px;
+                font-size: 15pt !important;
+            @endif
+            font-family: Calibri !important;
+            color:#f7371e!important;
+        ">
+
+            <table class="tb_footer">                    
+                <style>
+                    #decima td{
+                        font-family:calibri light;
+                        @if($requerimento->codigo_documento == 2 || $requerimento->codigo_documento == 10)
+                            padding-left:135px; 
+                        @else 
+                            padding-left:0px;
+                        @endif  
+                    }
+                    .iconeIMAg{ 
+                        height:18px;
+                        position:absolute;
+                        top:20px;
+                    }
+                </style>
                 
+                <tr id="decima">
                 @if($requerimento->codigo_documento == 2)
                  <td> 
                          {{$institution->morada}}, {{$institution->provincia}}, Angola
@@ -147,18 +166,8 @@
                 <td> 
                 Documento nº {{$requerimento->code ?? 'código doc'}} liquidado com CP nº{{$recibo ?? 'recibo'}} 
                 </td>
-                    
-                   
                 @endif
-                   {{-- <td  class="text-right" id="tel">  <img class="iconeIMAg" src="https://img.icons8.com/ios-glyphs/50/000000/filled-message.png" />
-                        {{$institution->email}} | <img class="iconeIMAg" src="https://img.icons8.com/ios-glyphs/60/000000/phone-office--v1.png"/>{{$institution->telefone_geral}} 
-                    </td> --}}      
-                </tr>
-                {{-- <tr>
-                    <td style="" colspan="1"></td></label>
-                    <td style="color:#444; text-align:center;font-size:11pt;" >powered by forLEARN<sup>®</sup> </td> 
-                    <td style="" colspan="1"></td>
-                </tr>  --}}     
+                </tr>    
             </table>
         </footer>
     </body>
