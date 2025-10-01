8e7cd246993f5484dcc20a99d122300c66c02b25
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-10-01 13:44:35 +0100

    style/ajustando a margem no ispk

--- app/Modules/Reports/Views/pdf_model/forLEARN_footer.blade.php
+++ app/Modules/Reports/Views/pdf_model/forLEARN_footer.blade.php
@@ -1,182 +1,110 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <script src="https://kit.fontawesome.com/8c60ea13ff.js" crossorigin="anonymous"></script>
-        <script>
-            function subst() {
-                var vars = {};
-                var x = document.location.search.substring(1).split('&');
-                for (var i in x) {
-                    var z = x[i].split('=', 2);
-                    vars[z[0]] = unescape(z[1]);
-                }
-                var x = ['frompage', 'topage', 'page', 'webpage', 'section', 'subsection', 'subsubsection'];
-                for (var i in x) {
-                    var y = document.getElementsByClassName(x[i]);
-                    for (var j = 0; j < y.length; ++j) y[j].textContent = vars[x[i]];
-                }
-            }
-        </script>
-        <style>
-            body{
-                margin: 0;
-                padding:0 0 40px 0;
-                color: #444;
-            }
-            footer{
-                margin-top: 50px;
-            
-            }
-            footer {
-                width: 100%;
-                position: fixed;     /* em vez de absolute */
-                bottom: 25px;        /* ajusta a altura desejada */
-                left: 0;
-                right: 0;
-                padding-top: 10px;
-                font-family: Calibri !important;
-                color: #f7371e !important;
-            }
+<head>
+  ...
+  <style>
+    /* corpo - reserva espaço para o footer na view */
+    html,body{
+      margin:0;
+      padding:0;
+      color:#444;
+      box-sizing:border-box;
+    }
+    body{
+      padding-bottom: 120px; /* reserva espaço quando estás a ver no browser */
+    }
 
-           @page {
-                margin-bottom: 70px; /* garante espaço reservado para o footer */
-            }
+    /* garantir espaço no PDF/print */
+    @page {
+      /* ajusta estes valores conforme precisares (mm é mais fiável para PDF) */
+      margin: 20mm 10mm 25mm 10mm; /* top right bottom left */
+    }
 
-            @media print {
-                footer {
-                    position: fixed;
-                    bottom: 25px;  /* altura que ficou boa para ti */
-                    left: 0;
-                    right: 0;
-                    text-align: center;
-                }
-            }
+    /* footer fixo por página/viewport (mais consistente em preview e PDF) */
+    footer.pdf-footer{
+      width: 100%;
+      position: fixed;   /* prende ao fundo da página */
+      left: 0;
+      right: 0;
+      bottom: 30px;      /* ajusta este valor para subir/baixar o texto */
+      z-index: 9999;
+      box-sizing: border-box;
+      padding: 6px 15mm; /* alinha com as margens da página */
+      font-family: Calibri, "Calibri Light", sans-serif !important;
+      color: #f7371e !important;
+      text-align: left;  /* muda para right/center se preferires */
+      pointer-events: none; /* evita interacções desnecessárias */
+    }
 
-            .tb_footer{
-                opacity:0.8;
-                width: 1000px;
-                
-            }
+    /* contenor interno do footer */
+    .tb_footer{
+      max-width: calc(100% - 30mm); /* deixa espaço para as margens laterais do @page */
+      margin: 0 auto;
+      width: auto; /* evita largura fixa que pode causar overflow */
+      opacity: 0.9;
+      overflow: visible; /* garante que nada é cortado internamente */
+      display: block;
+    }
 
-            .text-center {
-            
-                text-align: right;
-                /* padding-top:80px; */
-                padding:0;
+    /* evita que o texto seja cortado horizontalmente */
+    #decima td {
+      font-family: "Calibri Light", Calibri, sans-serif;
+      padding-left: 0;
+      white-space: normal;
+      word-break: break-word;
+    }
 
-            }
+    /* evita usar posicionamento absoluto nas iconices dentro do footer */
+    .iconeIMAg {
+      height: 18px;
+      position: relative; /* antes era absolute/top:20px e isso fazia overlap */
+      top: 0;
+      vertical-align: middle;
+      margin-right: 6px;
+    }
 
-            .text-right {
-                text-align:right;
-                
-            }
-            #tel{
-                /* widows:40px;  */
-                /* height:40px; */
-            }
-
-        
-        </style>
-    </head>
-
-    <body style="@if($requerimento->codigo_documento == 12)
-    padding-top:30px;
-    @endif">
-        <!--<footer style="@if($requerimento->codigo_documento != 2)margin-left:595px;@else margin-left:0px @endif font-size:22pt !important;font-family:calibri light !important;">-->
-<footer        style="
-@if($requerimento->codigo_documento == 2)
-    margin-left: -20px;
-    font-size: 11.5pt !important;
-@elseif($requerimento->codigo_documento == 10 || $requerimento->codigo_documento == 6)
-    margin-left: 150px;
-    font-size: 13pt !important;
-@elseif($requerimento->codigo_documento == 12)
-    margin-left: 30px;
-    font-size: 13pt !important;
-@elseif($requerimento->codigo_documento == 11)
-    margin-left: 150px;
-    font-size: 13pt !important;
-@else
-    margin-left: 595px;
-    font-size: 15pt !important;
-@endif
-    font-family: Calibri !important;
-    color:#f7371e!important;>
-    ">
-
-            <table class="tb_footer">
-                                    
-                <style>#decima td{font-family:calibri light;@if($requerimento->codigo_documento == 2 || $requerimento->codigo_documento == 10)padding-left:135px; @else padding-left:0px;@endif  }.iconeIMAg{ height:18px;position:absolute;top:20px;
-               }</style>
-                <tr id="decima">
-                
-                @if($requerimento->codigo_documento == 2)
-                 <td> 
-                         {{$institution->morada}}, {{$institution->provincia}}, Angola
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->dominio_internet}}
-                    </td>
-                @elseif($requerimento->codigo_documento == 11)
-                 <td> 
-                         {{$institution->morada}}, {{$institution->provincia}}, Angola
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;
-                         
-                         {{$institution->dominio_internet}}
-                    </td>
-                 @elseif($requerimento->codigo_documento == 10)
-                 
-                         {{$institution->morada}}, {{$institution->provincia}}, Angola
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->dominio_internet}}
-                 @elseif($requerimento->codigo_documento == 12)
-                 
-                         {{$institution->morada}}, {{$institution->provincia}}, Angola
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->dominio_internet}}
-                 @elseif($requerimento->codigo_documento == 6)
-                 
-                         {{$institution->morada}}, {{$institution->provincia}}, Angola
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                         {{$institution->dominio_internet}}
-                @else
-                <td> 
-                Documento nº {{$requerimento->code ?? 'código doc'}} liquidado com CP nº{{$recibo ?? 'recibo'}} 
-                </td>
-                    
-                   
-                @endif
-                   {{-- <td  class="text-right" id="tel">  <img class="iconeIMAg" src="https://img.icons8.com/ios-glyphs/50/000000/filled-message.png" />
-                        {{$institution->email}} | <img class="iconeIMAg" src="https://img.icons8.com/ios-glyphs/60/000000/phone-office--v1.png"/>{{$institution->telefone_geral}} 
-                    </td> --}}      
-                </tr>
-                {{-- <tr>
-                    <td style="" colspan="1"></td></label>
-                    <td style="color:#444; text-align:center;font-size:11pt;" >powered by forLEARN<sup>®</sup> </td> 
-                    <td style="" colspan="1"></td>
-                </tr>  --}}     
-            </table>
-        </footer>
-    </body>
-</html>
+    /* utilitário de debug — remove depois de validar */
+    /* .pdf-footer { outline: 1px dashed magenta; } */
+  </style>
+</head>
+<body>
+  ...
+  <!-- Footer corrigido (note a classe pdf-footer e style só para o font-size condicional) -->
+  <footer class="pdf-footer" style="
+    @if($requerimento->codigo_documento == 2)
+      font-size: 11.5pt !important;
+    @elseif($requerimento->codigo_documento == 10 || $requerimento->codigo_documento == 6 || $requerimento->codigo_documento == 11 || $requerimento->codigo_documento == 12)
+      font-size: 13pt !important;
+    @else
+      font-size: 15pt !important;
+    @endif
+  ">
+    <div class="tb_footer">
+      <table>
+        <tr id="decima">
+          <td style="@if($requerimento->codigo_documento == 2 || $requerimento->codigo_documento == 10) padding-left:135px; @else padding-left:0px; @endif">
+            @if($requerimento->codigo_documento == 2)
+              {{$institution->morada}}, {{$institution->provincia}}, Angola
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {{$institution->dominio_internet}}
+            @elseif($requerimento->codigo_documento == 11)
+              {{$institution->morada}}, {{$institution->provincia}}, Angola
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {{$institution->dominio_internet}}
+            @elseif($requerimento->codigo_documento == 10 || $requerimento->codigo_documento == 12 || $requerimento->codigo_documento == 6)
+              {{$institution->morada}}, {{$institution->provincia}}, Angola
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {{$institution->telemovel_geral}} | {{$institution->telefone_geral}}
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {{$institution->dominio_internet}}
+            @else
+              Documento nº {{$requerimento->code ?? 'código doc'}} liquidado com CP nº{{$recibo ?? 'recibo'}}
+            @endif
+          </td>
+        </tr>
+      </table>
+    </div>
+  </footer>
+</body>
