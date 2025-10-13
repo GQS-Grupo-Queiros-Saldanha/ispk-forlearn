f5bf5c0a0ee34c66e29269dc47c645de77eb7e09
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-10-13 13:57:59 +0100

    mudança de curso

--- app/Modules/GA/helper.php
+++ app/Modules/GA/helper.php
@@ -212,7 +212,7 @@ function EmolumentCodevLective($search, $lective_year_id)
         ->join('code_developer as code', 'code.id', '=', 'art.id_code_dev')
         ->select(['art.code', 'art.id as id_emolumento', 'art.anoLectivo as lectiveYear', 'code.code as codigo_dev'])
         ->where('code.code', $search)
-        ->whereIn('art.anoLectivo', [11]) //$lective_year_id->id
+        ->whereIn('art.anoLectivo', $lective_year_id) //$lective_year_id->id
         ->get();
     return $Consulta;
 }
