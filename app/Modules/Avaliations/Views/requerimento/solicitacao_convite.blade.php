5e12616fc28a103f2754a2fdabeb73ee4925c205
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-11-03 12:23:57 +0100

    feat/solicitação convite

--- app/Modules/Avaliations/Views/requerimento/solicitacao_convite.blade.php
+++ app/Modules/Avaliations/Views/requerimento/solicitacao_convite.blade.php
@@ -38,20 +38,6 @@
                     <div class="row">
                         <div class="col-6">
                             <div class="form-group col">
-                                <label>Selecionar curso</label>
-                                <select name="course_id" id="courses" class="selectpicker form-control form-control-sm">
-                                    <option value="" selected>Selecione o Curso</option>
-                                    @foreach ($courses as $course)
-                                        <option value="{{ $course->id }}">
-                                            {{ $course->currentTranslation->display_name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="col-6">
-                            <div class="form-group col">
                                 <label>Estudante</label>
                                 <select name="student_id" id="students" class="selectpicker form-control form-control-sm" data-live-search="true">
                                     <option value="" selected>Selecione o Estudante</option>
@@ -59,19 +45,9 @@
                                 </select>
                             </div>
                         </div>
-                        <!--Disciplinas-->
-                        <div class="col-6">
-                            <div class="form-group col">
-                                <label>Disciplinas</label>
-                                <select name="disciplina_id" id="disciplina" class="selectpicker form-control form-control-sm" data-live-search="true">
-                                    <option value="" selected>Seleciona a disciplina</option>
-                                    <!--Colocado pelo JS-->
-                                </select>
-                            </div>
-                        </div>
+                        
                     </div>
 
-                    <input type="hidden" id="lectiveY" value="{{ $lectiveYear->id }}" name="lective_year">
                 </div>
                 <hr>
                 <div class="float-right">
@@ -88,78 +64,7 @@
 @section('scripts')
     @parent
 <script>
-    const anoLectivo = $("#lectiveY");
 
-    // URLs
-    const estudantesUrl = "{{ url('/pt/avaliations/requerimento/getEstudante') }}";
-    const disciplinasUrl = "{{ url('/pt/avaliations/requerimento/getDisciplinas') }}";
-
-    // Atualiza o ano letivo escondido
-    $("#lective_year").change(function() {
-        anoLectivo.val($(this).val());
-    });
-
-    // Quando seleciona o curso -> buscar estudantes
-    $("#courses").change(function() {
-        const course_id = $(this).val();
-        const ano = $("#lective_year").val();
-
-        if (!course_id) return;
-
-        let url = `${estudantesUrl}/${course_id}/${ano}`;
-
-        fetch(url)
-            .then(response => response.json())
-            .then(dados => {
-                console.log("Estudantes recebidos:", dados);
-                const studentSelect = $("#students");
-                studentSelect.empty();
-                studentSelect.append(`<option value="">Selecione o Estudante</option>`);
-
-                dados.forEach(student => {
-                    studentSelect.append(
-                        `<option value="${student.user_id}" data-tokens="${student.name} ${student.student_number} ${student.email}">
-                            ${student.name ?? 'Sem nome'} #${student.student_number ?? ''}(${student.email ?? 'Sem email'})
-                        </option>`
-                    );
-                });
-
-                studentSelect.selectpicker('refresh');
-            })
-            .catch(err => console.error("Erro a buscar estudantes:", err));
-    });
-
-    // Quando seleciona o estudante -> buscar disciplinas
-    $("#students").change(function() {
-        const student_id = $(this).val();
-        const course_id = $("#courses").val()
-        const ano = $("#lective_year").val();
-
-        if (!student_id) return;
-
-        let url = `${disciplinasUrl}/${student_id}/${ano}/${course_id}`;
-        console.log("URL chamada:", url);
-
-        fetch(url)
-            .then(response => response.json())
-            .then(dados => {
-                console.log("Disciplinas recebidas:", dados);
-                const disciplinaSelect = $("#disciplina");
-                disciplinaSelect.empty();
-                disciplinaSelect.append(`<option value="">Seleciona a disciplina</option>`);
-
-                dados.forEach(disciplina => {
-                    disciplinaSelect.append(
-                        `<option value="${disciplina.id}" data-tokens="${disciplina.code} ${disciplina.name}">
-                            #(${disciplina.code}) - ${disciplina.name ?? 'Sem nome'} 
-                        </option>`
-                    );
-                });
-
-                disciplinaSelect.selectpicker('refresh');
-            })
-            .catch(err => console.error("Erro a buscar disciplinas:", err));
-    });
 </script>
 
 @endsection
