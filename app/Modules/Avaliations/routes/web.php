0b6e6da8f308ae096c278afd1315a542630bd349
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-11-03 14:18:56 +0100

    feat/solicitação convite

--- app/Modules/Avaliations/routes/web.php
+++ app/Modules/Avaliations/routes/web.php
@@ -433,7 +433,7 @@ Route::group(
                 
                 //Convite
                 Route::get('/requerimento/solicitacao_convite', [RequerimentoController::class, 'solicitacao_convite'])->name('requerimento.solicitacao_convite');
-                Route::get('/requerimento/create_convite', [RequerimentoController::class, 'create_convite'])->name('requerimento.create_convite');
+                Route:post('/requerimento/create_convite', [RequerimentoController::class, 'create_convite'])->name('requerimento.create_convite');
                 
                 //TFC
                 Route::get('requerimento/create_student_tfc', 'RequerimentoController@createStudentTfc')->name("create_student_tfc");
