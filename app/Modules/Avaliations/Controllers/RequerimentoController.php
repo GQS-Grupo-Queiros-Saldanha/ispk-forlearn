78942e6c96580ec238f0b0d158bd638ec94d522a
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-11-04 15:14:47 +0100

    rebaese

--- app/Modules/Avaliations/Controllers/RequerimentoController.php
+++ app/Modules/Avaliations/Controllers/RequerimentoController.php
@@ -624,6 +624,7 @@ class RequerimentoController extends Controller
                 ->join('article_translations as at', 'a.id', '=', 'at.article_id')
                 ->whereNull('a.deleted_at')
                 ->where('a.anoLectivo', $lective_year_id)
+                ->whereIn('a.id', [401,402]) // IDs dos tipos de convite
                 ->select(['a.id as id', 'at.display_name as name'])
                 ->get();
             return response()->json($invitation);
