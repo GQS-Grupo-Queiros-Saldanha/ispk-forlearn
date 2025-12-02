424334d890eaf841ab24d03fa2a54c04d073bc50
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-12-02 09:53:29 +0100

    fix(Relatorio de matriculas) estudantes de turmas diferentes aparecnedo para turma selecionada

--- app/Modules/Users/Controllers/MatriculationClasseListController.php
+++ app/Modules/Users/Controllers/MatriculationClasseListController.php
@@ -198,6 +198,7 @@ class MatriculationClasseListController extends Controller
             Toastr::error(__('Não foram encontrados alunos matriculados na turma selecionada.'), __('toastr.error'));
             return redirect()->back();
         }
+        dd($model)
 
         // 💰 Filtro de dívidas e bolsas (mantido)
         if (isset($request->status) && $request->status == "0") {
