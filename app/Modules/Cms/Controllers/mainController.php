a47e904bfcdbfff4b4af8c98993f4cd71679c101
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-12-08 11:15:09 +0100

    fix(Boletim) boletim do estudante não traz as notas para este ano!

--- app/Modules/Cms/Controllers/mainController.php
+++ app/Modules/Cms/Controllers/mainController.php
@@ -106,7 +106,7 @@ class mainController extends Controller
                  Log::info('CONFIG DEBUG3', ['config' => $config]);
                 $student = auth()->user()->id;
                 $matriculations = $this->get_matriculation_student(student: $student); //$this->get_matriculation_student($student);
-                $this->get_matriculation_student(student: $student);
+                
 
                 dd($matriculations);
                 $melhoria_notas = get_melhoria_notas($student, $lective->id, 0);
