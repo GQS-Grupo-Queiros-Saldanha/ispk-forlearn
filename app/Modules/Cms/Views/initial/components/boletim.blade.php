9406feb622a54050c297f6d70d9664c2328846d4
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2025-12-15 13:35:29 +0100

    fix(Boletim) NOtas fantasmas medias mal calculadas

--- app/Modules/Cms/Views/initial/components/boletim.blade.php
+++ app/Modules/Cms/Views/initial/components/boletim.blade.php
@@ -96,514 +96,189 @@ use App\Modules\Cms\Controllers\mainController;
  
 </style>
 
-
 @if (isset($articles['dividas']['pending']) && $articles['dividas']['pending'] > 0)
- <div class="alert alert-warning text-dark font-bold">Para visualizar as notas lançadas, dirija-se a Tesouraria para
- regularizar os seus pagamentos!</div>
-@else
- @if (is_object($percurso)) 
- @if (count($percurso) > 0)
 
- @php
- $semestres = ['1','2'];
- $disciplina_count = 0;
- 
- @endphp
- 
- @foreach($semestres as $semestreActual)
- <table class="table tabela_pauta table-striped table-hover tabela_pauta" id="{{ 'tabela_pauta_student' . $semestreActual }}">
- 
- <thead>
- <tr>
- 
- <td colspan="3" class="boletim_text">
+<div class="alert alert-warning text-dark font-bold">
+    Para visualizar as notas lançadas, dirija-se à Tesouraria para regularizar os seus pagamentos!
+</div>
 
- 
-
- @if (isset($matriculations->course))
- <b>{{ $matriculations->course }}</b>
- <as class="barra">|</as> Ano: <b>{{ $matriculations->course_year }}º</b>
- <as class="barra">|</as> Semestre: <b>{{ $semestreActual . 'º'}}</b>
- <as class="barra">|</as> Turma: <b>{{ $matriculations->classe }}</b>
- @endif
- </td>
- <td colspan="5" class="text-center bgmac bo1 p-top" style="border-bottom: 1px solid white; ">MAC
- </td>
- <td colspan="2" class="text-center bg1 p-top">EXAME</td>
- <td class="text-center cf1 bo1 p-top" colspan="2">CLASSIFICAÇÃO</td>
- <td class="rec bo1 text-center p-top" colspan="4">EXAME</td>
- <td class="fn bo1 text-center p-top" colspan="2">CLASSIFICAÇÃO</td>
- </tr>
- <tr style="text-align: center">
- <th class="bg1 bo1">#</th>
- <th class="text-center small bg1 bo1">CÓDIGO</th>
- <th class="bg1 bo1">DISCIPLINA</th>
- <th class="bgmac bo1">PF1</th>
- <th class="bgmac bo1">PF2</th>
- <th class="bgmac bo1">OA</th>
- <th colspan="2" class="bgmac bo1">MÉDIA</th>
- <th class="bg1 bo1">ESCRITO</th>
- <th class="bg1 bo1">ORAL</th>
- <th class="cf1 bo1" colspan="2">MAC + EXAME</th>
- <th class="rec bo1" colspan="2">RECURSO</th>
- <th class="rec bo1" colspan="2">ESPECIAL</th>
- <th class="fn bo1" colspan="2">FINAL</th>
- </tr>
- </thead>
- @foreach ($disciplines as $index => $item_DISC)
-
- @if($index[3] == $semestreActual)
- @php 
- $disciplina_count++;
-
- $par = null;
-
- if ($disciplina_count % 2 == 0) {
- $par = 'bg-white';
- }
-
-
- $disciplina_nome = $index;
- $avalicao_nome = null;
- $avaliacao_nota = 0;
- $code_disc = false;
- $avaliacao_count = 0;
- $pf1_nota = null;
- $pf1_percentagem = 0;
- $pf2_nota = null;
- $pf2_percentagem = 0;
- $oa_nota = null;
- $oa_percentagem = 0;
- $neen_nota = null;
- $oral_nota = null;
- $recurso_nota = null;
- $especial_nota = null;
- $classificacao = 0;
- $aval_mac = null;
- $mac_nota = 0;
- $estado_final = '';
- $count_exame = 0;
- $last_exame = 0;
- $nota_final = '-';
- $color_final = '';
- $mac_percentagem = $config->percentagem_mac / 100;
- $neen_percentagem = $config->percentagem_oral / 100;
- 
-$id_turma = $classes->first(function($item) use ($item_DISC) {
-    return $item_DISC->turma == $item->display_name; // ou $item->code
-})->id;
-
- $aprovado = false;
- $recurso = false;
- $exame = false;
- $exame_oral = false;
- $exam_only = $item_DISC->e_f;
- 
- @endphp
-
- <tbody>
- <tr class="{{'semestre'.$semestreActual}} {{ $par ?? '' }}">
- 
- <td style='text-align: center'>{{ $disciplina_count }}</td>
- <td style='text-align: center'>{{ $disciplina_nome }}</td>
- <td style='text-align: left'>{{ $item_DISC->nome_disciplina }}</td>
-
- @php
- if(isset($percurso[$index])){
- foreach ($percurso[$index] as $indexNotas => $itemNotas){
- if ($itemNotas->MT_CodeDV == 'PF1') {
- if ($itemNotas->nota_anluno == null) {
- 
- } else {
- $pf1_nota = floatval($itemNotas->nota_anluno);
- $pf1_percentagem = $itemNotas->percentagem_metrica / 100;
- }
- }
-
- if ($itemNotas->MT_CodeDV == 'PF2') {
- if ($itemNotas->nota_anluno == null) {
- } else {
- $pf2_nota = floatval($itemNotas->nota_anluno);
- $pf2_percentagem = $itemNotas->percentagem_metrica / 100;
- }
- }
-
- if ($itemNotas->MT_CodeDV == 'OA') {
- if ($itemNotas->nota_anluno == null) {
- } else {
- $oa_nota = floatval($itemNotas->nota_anluno);
- $oa_percentagem = $itemNotas->percentagem_metrica / 100;
- }
- }
-
- if ($itemNotas->MT_CodeDV == 'Neen') {
- if ($itemNotas->nota_anluno == null) {
- $neen_nota = 0;
- } else {
- $neen_nota = floatval($itemNotas->nota_anluno);
- }
- }
- if ($itemNotas->MT_CodeDV == 'oral') {
- if ($itemNotas->nota_anluno == null) {
- $oral_nota = 0;
- } else {
- $oral_nota = floatval($itemNotas->nota_anluno);
- }
- }
-
- if ($itemNotas->MT_CodeDV == 'Recurso') {
- if ($itemNotas->nota_anluno == null) {
- $recurso_nota = 0;
- } else {
- $recurso_nota = floatval($itemNotas->nota_anluno);
- $recurso_nota = round($recurso_nota);
- }
- }
-
- if ($itemNotas->MT_CodeDV == 'Exame_especial') {
- if ($itemNotas->nota_anluno == null) {
- $especial_nota = 0;
- } else {
- $especial_nota = floatval($itemNotas->nota_anluno);
- }
- }
- 
- }
- }
- @endphp
-
- <td class='text-bold text-center'>{{ isset($pf1_nota) ? number_format($pf1_nota, 2) : '-' }}</td>
- <td class='text-bold text-center'>{{ isset($pf2_nota) ? number_format($pf2_nota, 2) : '-' }}</td>
- <td class='text-bold text-center'>{{ isset($oa_nota) ? number_format($oa_nota, 2) : '-' }}</td>
-
- @php
-    
-    $p_mac = mainController::verificar_pauta(
-        $id_turma,
-        $item_DISC->id_disciplina,
-        $item_DISC->id_anoLectivo,
-        'Pauta Frequência'
-    );
-
-    // Caso tenha a nota de MAC lançada!
-
-    if ($p_mac > 0) {
-        
-            $mac_nota = (($pf1_nota * $pf1_percentagem) + ($pf2_nota * $pf2_percentagem) + ($oa_nota * $oa_percentagem));
-            $mac_nota = round($mac_nota);
-            $classificacao = $mac_nota;
-
-            if($exam_only == 1){
-                $exame = true;
-            }
-            else{
-                if ($classificacao >= 0 && $classificacao <= $config->mac_nota_recurso) {
-                $estado_final = 'Recurso';
-                $color_final = 'for-red';
-                $recurso = true;
-            }
-
-            if ($classificacao >= $config->exame_nota_inicial && $classificacao <= $config->exame_nota_final) {
-                $estado_final = 'Exame';
-                $color_final = 'for-yellow';
-                $exame = true;
-            }
-
-            if ($classificacao >= $config->mac_nota_dispensa && $classificacao <= 20) {
-                $estado_final = 'Aprovado(a)';
-                $color_final = 'for-green';
-                $aprovado = true;
-                
-            }
-
-            $nota_final = $classificacao;
-        }
-        
-    }
- @endphp
-
- @if ($p_mac > 0)
-    <td class='text-bold text-center'>{{ $nota_final }}</td>
-    <td class="{{'text-bold text-center ' .$color_final }}"> {{$estado_final}} </td>
- @else
-    <td style='text-align: center'>-</td>
-    <td style='text-align: center'>-</td>
- @endif
-
- @if ($neen_nota == null || $aprovado) 
-    <td style='text-align: center'> - </td>
- @else 
- 
- @if ($estado_final == 'Aprovado(a)') 
-    <td style='text-align: center'>-</td>
- 
- @elseif ($estado_final == 'Recurso')
-    <td style='text-align: center'>-</td>
- @else
-    <td style='text-align: center'>{{ round($neen_nota) }}</td>
- @endif
-
- @endif
+@elseif (is_object($percurso) && count($percurso) > 0)
 
 @php
-    if($exame){
-    ++$count_exame;
-    if ($neen_nota == null)
-    $neen_nota = 0;
-    else 
-    $neen_nota = round($neen_nota);
+    $semestres = ['1','2'];
+@endphp
 
-    if(!is_null($config->exame_oral_final) && ($neen_nota > $config->mac_nota_recurso && $neen_nota <= round($config->exame_oral_final)))
-    {
-    $exame_oral = true;
+@foreach ($semestres as $semestreActual)
+
+<table class="table tabela_pauta table-striped table-hover" id="tabela_pauta_student{{ $semestreActual }}">
+
+<thead>
+<tr>
+    <td colspan="3" class="boletim_text">
+        <b>{{ $matriculations->course }}</b>
+        <span class="barra">|</span> Ano: <b>{{ $matriculations->course_year }}º</b>
+        <span class="barra">|</span> Semestre: <b>{{ $semestreActual }}º</b>
+        <span class="barra">|</span> Turma: <b>{{ $matriculations->classe }}</b>
+    </td>
+    <td colspan="5" class="text-center bgmac">MAC</td>
+    <td colspan="2" class="text-center bg1">EXAME</td>
+    <td colspan="2" class="text-center cf1">CLASSIFICAÇÃO</td>
+    <td colspan="4" class="text-center rec">EXAME</td>
+    <td colspan="2" class="text-center fn">FINAL</td>
+</tr>
+
+<tr class="text-center">
+    <th>#</th>
+    <th>CÓDIGO</th>
+    <th>DISCIPLINA</th>
+    <th>PF1</th>
+    <th>PF2</th>
+    <th>OA</th>
+    <th colspan="2">MÉDIA</th>
+    <th>ESCRITO</th>
+    <th>ORAL</th>
+    <th colspan="2">MAC + EXAME</th>
+    <th colspan="2">RECURSO</th>
+    <th colspan="2">ESPECIAL</th>
+    <th colspan="2">FINAL</th>
+</tr>
+</thead>
+
+<tbody>
+
+@php $disciplina_count = 0; @endphp
+
+@foreach ($disciplines as $codigo => $disciplina)
+
+@if ($codigo[3] != $semestreActual)
+    @continue
+@endif
+
+@php
+    $disciplina_count++;
+
+    /* ===== NOTAS ===== */
+    $pf1 = $pf2 = $oa = $exame = $oral = $recurso = $especial = null;
+
+    if (isset($percurso[$codigo])) {
+        foreach ($percurso[$codigo] as $nota) {
+            if ($nota->nota_anluno === null) continue;
+
+            $valor = round($nota->nota_anluno, 2);
+
+            match ($nota->MT_CodeDV) {
+                'PF1' => $pf1 = $valor,
+                'PF2' => $pf2 = $valor,
+                'OA' => $oa = $valor,
+                'Neen' => $exame = $valor,
+                'oral' => $oral = $valor,
+                'Recurso' => $recurso = $valor,
+                'Exame_especial' => $especial = $valor,
+                default => null
+            };
+        }
     }
-    else{
-    if($exam_only == 1){
-    $classificacao = $neen_nota;
+
+    /* ===== ESTADO ===== */
+    $mac = $classificacao_final = null;
+    $estado = $cor = null;
+
+    /* ===== MAC ===== */
+    if ($pf1 !== null && $pf2 !== null && $oa !== null) {
+        $mac = round(
+            ($pf1 * ($config->pf1_percentagem / 100)) +
+            ($pf2 * ($config->pf2_percentagem / 100)) +
+            ($oa  * ($config->oa_percentagem  / 100))
+        );
+
+        if ($mac >= $config->mac_nota_dispensa) {
+            $classificacao_final = $mac;
+            $estado = 'Aprovado(a)';
+            $cor = 'for-green';
+        }
     }
-    else
-    {
-    $classificacao = ($mac_nota * $mac_percentagem) + ($neen_nota * $neen_percentagem);
+
+    /* ===== EXAME ===== */
+    if ($classificacao_final === null && $mac !== null && $exame !== null) {
+        $nota = round(
+            ($mac * ($config->percentagem_mac / 100)) +
+            ($exame * ($config->percentagem_oral / 100))
+        );
+
+        if ($nota >= 10) {
+            $classificacao_final = $nota;
+            $estado = 'Aprovado(a)';
+            $cor = 'for-green';
+        } else {
+            $estado = 'Recurso';
+            $cor = 'for-red';
+        }
     }
-    
 
+    /* ===== ORAL ===== */
+    if ($estado === 'Recurso' && $oral !== null && $mac !== null) {
+        $nota = round(
+            ($mac * ($config->percentagem_mac / 100)) +
+            ($oral * ($config->percentagem_oral / 100))
+        );
 
-    $classificacao = round($classificacao);
-    if ($classificacao >= 0 && $classificacao < $config->exame_nota) {
-    $estado_final = 'Recurso';
-    $color_final = 'for-red';
+        if ($nota >= 10) {
+            $classificacao_final = $nota;
+            $estado = 'Aprovado(a)';
+            $cor = 'for-green';
+        }
     }
 
-    if ($classificacao >= $config->exame_nota && $classificacao <= 20) {
-    $estado_final = 'Aprovado(a)';
-    $color_final = 'for-green';
+    /* ===== RECURSO ===== */
+    if ($estado === 'Recurso' && $recurso !== null) {
+        if ($recurso >= 10) {
+            $classificacao_final = $recurso;
+            $estado = 'Aprovado(a)';
+            $cor = 'for-green';
+        } else {
+            $estado = 'Especial';
+            $cor = 'for-red';
+        }
     }
-    $nota_final = round($classificacao);
+
+    /* ===== ESPECIAL ===== */
+    if ($especial !== null) {
+        $classificacao_final = $especial;
+        $estado = $especial >= 10 ? 'Aprovado(a)' : 'Reprovado(a)';
+        $cor = $especial >= 10 ? 'for-green' : 'for-red';
     }
-    
-    }
- @endphp
- 
- @if ($oral_nota == null)
- <td style='text-align: center'> - </td>
- @else
- @php
- $p_exame_oral = mainController::verificar_pauta(
- $id_turma,
- $item_DISC->id_disciplina,
- $item_DISC->id_anoLectivo,
- 'Pauta de Exame Oral',
- ); 
- @endphp
- 
- @if ($p_exame_oral > 0) 
- @if ($estado_final == 'Aprovado(a)') 
- <td style='text-align: center'>---</td>
- 
- @elseif ($estado_final == 'Recurso')
- <td style='text-align: center'>--</td>
- @else
- <td style='text-align: center'>{{$oral_nota}}</td>
- @endif
- @else
- <td style='text-align: center'>-</td> 
- @endif
+@endphp
 
+<tr>
+    <td>{{ $disciplina_count }}</td>
+    <td>{{ $codigo }}</td>
+    <td>{{ $disciplina->nome_disciplina }}</td>
 
- @endif
+    <td>{{ $pf1 ?? '-' }}</td>
+    <td>{{ $pf2 ?? '-' }}</td>
+    <td>{{ $oa ?? '-' }}</td>
 
- @php
- if($exame_oral){
- 
- 
- ++$count_exame;
- if ($oral_nota == null)
- $oral_nota = 0;
- else
- $oral_nota = round($oral_nota);
+    <td colspan="2">{{ $mac ?? '-' }}</td>
 
- if($exam_only == 1){
- $classificacao = $neen_nota;
- }
- else{
- $classificacao = ($mac_nota * $mac_percentagem) + ($oral_nota * $neen_percentagem);
- $classificacao = round($classificacao);
- }
+    <td>{{ $exame ?? '-' }}</td>
+    <td>{{ $oral ?? '-' }}</td>
 
- 
- 
- if ($classificacao >= 0 && $classificacao < 10) {
- $estado_final = 'Recurso';
- $color_final = 'for-red';
- }
- if ($classificacao >= 10 && $classificacao <= 20) {
- $estado_final = 'Aprovado(a)';
- $color_final = 'for-green';
- }
- 
- $nota_final = $classificacao;
- 
- }
- @endphp
+    <td colspan="2">{{ $classificacao_final ?? '-' }}</td>
 
+    <td colspan="2">{{ $recurso ?? '-' }}</td>
+    <td colspan="2">{{ $especial ?? '-' }}</td>
 
- @php
+    <td colspan="2" class="{{ $cor }}">{{ $estado ?? '-' }}</td>
+</tr>
 
- $p_final = mainController::verificar_pauta(
- $id_turma,
- $item_DISC->id_disciplina,
- $item_DISC->id_anoLectivo,
- 'Pauta Final',
- );
+@endforeach
+</tbody>
+</table>
 
- @endphp
+@endforeach
 
- @if($p_final > 0)
- @if($aprovado
- || $recurso) 
- <td class='text-bold text-center'>{{ $classificacao }}</td>
- <td class="{{'text-bold text-center ' . $color_final}}">{{ $estado_final }}</td>
- @else
- 
- <td class='text-bold text-center'>{{ $classificacao }}</td>
- <td class="{{'text-bold text-center ' . $color_final}}">{{ $estado_final }}</td>
- @endif
- @else
- <td style='text-align: center'>-</td>
- <td style='text-align: center'>-</td>
- @endif
-
- @if ($recurso_nota == null) 
- <td style='text-align: center'> - </td>
- <td style='text-align: center'> - </td> 
- @else
- @php
- $p_recurso = mainController::verificar_pauta(
- $id_turma,
- $item_DISC->id_disciplina,
- $item_DISC->id_anoLectivo,
- 'Pauta de Recurso',
- );
- @endphp
- 
- @if ($p_recurso > 0)
- @if ($estado_final == 'Aprovado(a)') 
- <td style='text-align: center'>-</td>
- <td style='text-align: center'>-</td>
- @else 
- @php
- $classificacao = $recurso_nota;
-
- $classificacao = round($classificacao);
-
- if ($classificacao >= 0 && $classificacao < 10) {
- $estado_final = 'Especial';
- $color_final = 'for-red';
- }
- if ($classificacao >= 10 && $classificacao <= 20) {
- $estado_final = 'Aprovado(a)';
- $color_final = 'for-green';
- }
- $nota_final = $classificacao;
- @endphp
- <td style='text-align: center'>{{ $recurso_nota }}</td>
- <td class="{{'text-bold text-center ' . $color_final}}">{{ $estado_final }}</td>
- @endif
- @else
- <td style='text-align: center'>-</td>
- <td style='text-align: center'>-</td>
- @endif
-
- 
- 
- @endif
-
- @if ($especial_nota == null)
- <td style='text-align: center'> - </td>
- <td style='text-align: center'> - </td>
- @else
- @php
-
- $p_especial = mainController::verificar_pauta(
- $id_turma,
- $item_DISC->id_disciplina,
- $item_DISC->id_anoLectivo,
- 'Pauta Exame Especial',
- );
-
- @endphp
-
- @if ($p_especial > 0)
- @if ($estado_final == 'Aprovado(a)') 
- <td style='text-align: center'>-</td>
- <td style='text-align: center'>-</td>
- @else
- @php
- $classificacao = round($especial_nota);
-
- if ($classificacao >= 0 && $classificacao < 10) {
- $estado_final = 'Reprovado(a)';
- $color_final = 'for-red';
- }
-
- if ($classificacao >= 10 && $classificacao <= 20) {
- $estado_final = 'Aprovado(a)';
- $color_final = 'for-green';
- }
- $nota_final = $classificacao;
- @endphp
-
- <td style='text-align: center'>{{ $especial_nota }}</td>
- <td class="{{'text-bold text-center ' . $color_final}}">{{ $estado_final }}</td>
- @endif
-
- @else
- <td style='text-align: center'>-</td>
- @endif
- @endif
-
- 
-
- 
-
- @if ($p_final > 0)
- @if ($estado_final == 'Aprovado(a)')
- <td class='text-bold text-center '>{{$nota_final}}</td>
- <td class="{{'text-bold text-center ' .$color_final }}"> {{$estado_final}} </td>
- @else
- <td class='text-bold text-center '>{{ $nota_final }}</td>
- <td class="{{'text-bold text-center ' .$color_final }}">{{$estado_final}}</td>
- @endif
-
- @else
- <td class='text-bold text-center'>-</td>
- <td class='text-bold text-center'>-</td>
- @endif
-
- 
-
- </tr>
-
- </tbody>
-
- @endif
- 
-
- @endforeach
- </table>
- @endforeach
- 
- @if(!isset($institution)) 
- <div class="row float-right btn-pdf-boletim" style="margin-right: 0.1!important;">
- <a class="btn " style="background-color:#0082f2;" target="_blank" href="{{ route("main.boletim_pdf", $matriculations->id) }}">
- <i class="fa fa-file-pdf"></i> Boletim de notas</a> 
- </div>
- @endif
- 
- @else
- <div class="alert alert-warning text-dark font-bold">Nenhuma nota foi lançada neste ano lectivo! </div>
- @endif
- @else
- <div class="alert alert-warning text-dark font-bold"> {{ $percurso }}!</div>
- @endif
+@else
+<div class="alert alert-warning text-dark font-bold">
+    Nenhuma nota foi lançada neste ano lectivo!
+</div>
 @endif
