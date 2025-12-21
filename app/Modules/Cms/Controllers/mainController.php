c9a7858f8ddaf02b1a7b540b5d751b9dd2910b5b
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-12-21 19:21:34 +0100

    home office

--- app/Modules/Cms/Controllers/mainController.php
+++ app/Modules/Cms/Controllers/mainController.php
@@ -1107,7 +1107,7 @@ class mainController extends Controller
             )
             ->orderBy('mc.id_sui', 'desc') // maior id primeiro
             ->first();
-        dd($matricula);
+        Log::info('matricula',['dados'=>$matricula]);
         /*-----------------------------------*/
         $disciplinas = DB::table('matriculation_disciplines as md')
             ->join('disciplines as d', 'd.id', '=', 'md.discipline_id')
