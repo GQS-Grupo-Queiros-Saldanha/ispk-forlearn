9deea48d7c5617fb7a63c2a3954e54f5cdd6776d
Ezequiel <ezequiel.p.a.francisco@gmail.com>
2025-12-21 15:39:58 +0100

    home office

--- app/Modules/Cms/Views/initial/components/boletim.blade.php
+++ app/Modules/Cms/Views/initial/components/boletim.blade.php
@@ -32,118 +32,135 @@ use App\Modules\Cms\Controllers\mainController;
 @elseif (auth()->check() && auth()->user()->id != 529)
     @include('Cms::initial.components.manutencao')
 @else
+    
     @if(isset($disciplinas) && count($disciplinas))
-        @php
-            // Separar disciplinas por semestre
-            $disciplinas_semestre1 = [];
-            $disciplinas_semestre2 = [];
-            foreach($disciplinas as $d){
-                $sem = intval($d->disciplinas[3]);
-                if($sem === 1) $disciplinas_semestre1[] = $d;
-                elseif($sem === 2) $disciplinas_semestre2[] = $d;
-            }
-            $semestres = [1 => $disciplinas_semestre1, 2 => $disciplinas_semestre2];
-        @endphp
 
-        @foreach($semestres as $num_semestre => $disciplinas_semestre)
-            @if(count($disciplinas_semestre))
-                <table class="table tabela_pauta table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <td colspan="3" class="boletim_text">
-                                <b>{{ $matricula->nome_curso }}</b>
-                                <as class="barra">|</as> Ano: <b>{{ $matricula->ano_curricular }}º</b>
-                                <as class="barra">|</as> Semestre: <b>{{ $num_semestre }}º</b>
-                                <as class="barra">|</as> Turma: <b>{{ $matricula->nome_turma }}</b>
-                            </td>
-                            <td colspan="5" class="text-center bgmac bo1 p-top">MAC</td>
-                            <td colspan="2" class="text-center bg1 p-top">EXAME</td>
-                            <td colspan="2" class="text-center cf1 bo1 p-top">CLASSIFICAÇÃO</td>
-                            <td colspan="4" class="rec bo1 text-center p-top">EXAME</td>
-                            <td colspan="2" class="fn bo1 text-center p-top">CLASSIFICAÇÃO</td>
-                        </tr>
-                        <tr style="text-align: center">
-                            <th class="bg1 bo1">#</th>
-                            <th class="bg1 bo1">CÓDIGO</th>
-                            <th class="bg1 bo1">DISCIPLINA</th>
-                            <th class="bgmac bo1">PF1</th>
-                            <th class="bgmac bo1">PF2</th>
-                            <th class="bgmac bo1">OA</th>
-                            <th colspan="2" class="bgmac bo1">MÉDIA</th>
-                            <th class="bg1 bo1">ESCRITO</th>
-                            <th class="bg1 bo1">ORAL</th>
-                            <th colspan="2" class="cf1 bo1">MAC + EXAME</th>
-                            <th colspan="2" class="rec bo1">RECURSO</th>
-                            <th colspan="2" class="rec bo1">ESPECIAL</th>
-                            <th colspan="2" class="fn bo1">FINAL</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($disciplinas_semestre as $index => $disciplina)
-                            @php
-                                // Pegar notas de cada métrica
-                                $pf1 = $pf2 = $oa = $ex_escrito = $ex_oral = 0;
+    @php
+        $disciplinas_semestre1 = [];
+        $disciplinas_semestre2 = [];
 
-                                foreach($dados as $nota){
-                                    if($nota->disciplina == $disciplina->disciplinas){
-                                        if($nota->metrica == 'PP1') $pf1 = $nota->nota;
-                                        if($nota->metrica == 'PP2') $pf2 = $nota->nota;
-                                        if($nota->metrica == 'OA') $oa = $nota->nota;
-                                        if($nota->metrica == 'Exame Escrito') $ex_escrito = $nota->nota;
-                                        if($nota->metrica == 'Exame Oral') $ex_oral = $nota->nota;
-                                    }
-                                }
+        foreach($disciplinas as $d){
+            $sem = intval($d->disciplinas[3]);
+            if($sem === 1) $disciplinas_semestre1[] = $d;
+            if($sem === 2) $disciplinas_semestre2[] = $d;
+        }
 
-                                // Média ponderada PF1+PF2+OA
-                                $media = round(($pf1*0.35)+($pf2*0.35)+($oa*0.3),2);
+        $semestres = [
+            1 => $disciplinas_semestre1,
+            2 => $disciplinas_semestre2
+        ];
+    @endphp
 
-                                // Exame total
-                                $exame_total = ($ex_escrito ? $ex_escrito : 0) + ($ex_oral ? $ex_oral : 0);
-                                $media_exame = $exame_total ? round(($media*0.7) + ($exame_total*0.3),2) : $media;
+    @foreach($semestres as $num_semestre => $disciplinas_semestre)
 
-                                // Cor e classificação
-                                if($media >= 10.30){
-                                    $cor_media = 'for-green';
-                                    $classificacao = 'Aprovado(a)';
-                                } elseif($media == 10 && $media < 10.29  ){
-                                    $cor_media = 'for-yellow';
-                                    $classificacao = 'Exame';
-                                } else{
-                                    $cor_media = 'for-red';
-                                    $classificacao = 'Recurso';
-                                }
-                            @endphp
+        @if(count($disciplinas_semestre))
 
-                            <tr>
-                                <td class="text-center">{{ $index + 1 }}</td>
-                                <td class="text-center">{{ $disciplina->disciplinas }}</td>
-                                <td>{{ $disciplina->nome_disciplina }}</td>
+            <table class="table tabela_pauta table-striped table-hover">
+            <thead>
+            <tr>
+                <td colspan="3" class="boletim_text">
+                    <b>{{ $matricula->nome_curso }}</b>
+                    <span class="barra">|</span> Ano: <b>{{ $matricula->ano_curricular }}º</b>
+                    <span class="barra">|</span> Semestre: <b>{{ $num_semestre }}º</b>
+                    <span class="barra">|</span> Turma: <b>{{ $matricula->nome_turma }}</b>
+                </td>
+                <td colspan="5" class="text-center bgmac bo1">MAC</td>
+                <td colspan="2" class="text-center bg1">EXAME</td>
+                <td colspan="2" class="text-center cf1 bo1">CLASSIFICAÇÃO</td>
+            </tr>
 
-                                <td class="text-center">{{ $pf1 }}</td>
-                                <td class="text-center">{{ $pf2 }}</td>
-                                <td class="text-center">{{ $oa }}</td>
+            <tr class="text-center">
+                <th class="bg1 bo1">#</th>
+                <th class="bg1 bo1">CÓDIGO</th>
+                <th class="bg1 bo1">DISCIPLINA</th>
+                <th class="bgmac bo1">PF1</th>
+                <th class="bgmac bo1">PF2</th>
+                <th class="bgmac bo1">OA</th>
+                <th class="bgmac bo1">MÉDIA</th>
+                <th class="bg1 bo1">ESCRITO</th>
+                <th class="bg1 bo1">ORAL</th>
+                <th class="cf1 bo1">FINAL</th>
+                <th class="cf1 bo1">ESTADO</th>
+            </tr>
+            </thead>
 
-                                <td class="text-center">{{ $media }}</td>
-                                <td class="text-center {{ $cor_media }}">{{ $classificacao }}</td>
+            <tbody>
 
-                                <td class="text-center">{{ $ex_escrito ?: '-' }}</td>
-                                <td class="text-center">{{ $ex_oral ?: '-' }}</td>
+            @foreach($disciplinas_semestre as $index => $disciplina)
 
-                                <td class="text-center">{{ $media_exame }}</td>
-                                <td class="text-center {{ $cor_media }}">{{ $classificacao }}</td>
+            @php
+                $pf1 = $pf2 = $oa = $ex_escrito = $ex_oral = null;
 
-                                <td colspan="2" class="text-center">-</td>
-                                <td colspan="2" class="text-center">-</td>
+                foreach($dados as $nota){
+                    if($nota->disciplina == $disciplina->disciplinas){
+                        if($nota->metrica == 'PP1') $pf1 = $nota->nota;
+                        if($nota->metrica == 'PP2') $pf2 = $nota->nota;
+                        if($nota->metrica == 'OA')  $oa  = $nota->nota;
+                        if($nota->metrica == 'Exame Escrito') $ex_escrito = $nota->nota;
+                        if($nota->metrica == 'Exame Oral')    $ex_oral    = $nota->nota;
+                    }
+                }
 
-                                <td colspan="2" class="text-center">{{ $media_exame }}</td>
-                                <td colspan="2" class="text-center {{ $cor_media }}">{{ $classificacao }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @endif
-        @endforeach
+                // MAC
+                $media = round(
+                    ($pf1 * 0.35) +
+                    ($pf2 * 0.35) +
+                    ($oa  * 0.30)
+                , 2);
+
+                // Exame
+                $exame_total = 0;
+                if($ex_escrito !== null) $exame_total += $ex_escrito;
+                if($ex_oral !== null)    $exame_total += $ex_oral;
+
+                // Nota final REAL
+                if($exame_total > 0){
+                    $nota_final = round(($media * 0.7) + ($exame_total * 0.3), 2);
+                } else {
+                    $nota_final = $media;
+                }
+
+                // Classificação FINAL
+                if($nota_final >= 10.30){
+                    $cor = 'for-green';
+                    $estado = 'Aprovado(a)';
+                } elseif($nota_final >= 10 && $nota_final < 10.30){
+                    $cor = 'for-yellow';
+                    $estado = 'Exame';
+                } else {
+                    $cor = 'for-red';
+                    $estado = 'Recurso';
+                }
+            @endphp
+
+            <tr>
+                <td class="text-center">{{ $index + 1 }}</td>
+                <td class="text-center">{{ $disciplina->disciplinas }}</td>
+                <td>{{ $disciplina->nome_disciplina }}</td>
+
+                <td class="text-center">{{ $pf1 ?? '-' }}</td>
+                <td class="text-center">{{ $pf2 ?? '-' }}</td>
+                <td class="text-center">{{ $oa  ?? '-' }}</td>
+
+                <td class="text-center">{{ $media }}</td>
+
+                <td class="text-center">{{ $ex_escrito ?? '-' }}</td>
+                <td class="text-center">{{ $ex_oral ?? '-' }}</td>
+
+                <td class="text-center">{{ $nota_final }}</td>
+                <td class="text-center {{ $cor }}">{{ $estado }}</td>
+            </tr>
+
+            @endforeach
+
+            </tbody>
+            </table>
+
+        @endif
+    
+    @endforeach
+
     @else
-        <h1>Sem disciplinas associadas à matrícula</h1>
+        <h3>Sem disciplinas associadas à matrícula</h3>
     @endif
 @endif
