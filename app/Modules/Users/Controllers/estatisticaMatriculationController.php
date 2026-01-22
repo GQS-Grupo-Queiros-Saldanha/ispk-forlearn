7fd8370d7368e5e3e24a7b683c031fc6fde60d35
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2026-01-22 16:05:49 +0100

    fix(relatorio estatistico) erro dados não corroboram

--- app/Modules/Users/Controllers/estatisticaMatriculationController.php
+++ app/Modules/Users/Controllers/estatisticaMatriculationController.php
@@ -127,7 +127,7 @@ class estatisticaMatriculationController extends Controller
             ->first();
 
         $emolumento_confirma_prematricula = $this->pre_matricula_confirma_emolumento($lectiveYears->id);
-        //dd($emolumento_confirma_prematricula);
+        dd($emolumento_confirma_prematricula,$lectiveYears->id);
 
 
 
