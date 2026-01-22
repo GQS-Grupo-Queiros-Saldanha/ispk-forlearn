7b4e65034d3725844589b647e8662dcafb277234
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2026-01-22 11:16:09 +0100

    style(PDF_Lista de matriculas) adicionando metrica

--- app/Modules/Cms/Controllers/mainController.php
+++ app/Modules/Cms/Controllers/mainController.php
@@ -1337,6 +1337,7 @@ class mainController extends Controller
             )
             ->orderBy('mc.id_sui', 'desc') // maior id primeiro
             ->get();
+            dd($matricula);
         /*-----------------------------------*/
         $disciplinas = DB::table('matriculation_disciplines as md')
             ->join('disciplines as d', 'd.id', '=', 'md.discipline_id')
