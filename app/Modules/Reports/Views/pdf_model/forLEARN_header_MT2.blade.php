32d7890e8924b1680bec4d64f98ce94f9f7a30fd
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2026-01-22 12:42:57 +0100

    style(PDF_Lista de matriculas) adicionando metrica

--- app/Modules/Reports/Views/pdf_model/forLEARN_header_MT2.blade.php
+++ app/Modules/Reports/Views/pdf_model/forLEARN_header_MT2.blade.php
@@ -1,5 +1,5 @@
+
 <style>
-    /* Mantenho todo o CSS original */
     body {
         font-family: 'Calibri Light', sans-serif;
     }
@@ -29,6 +29,7 @@
     }
 
     .h1-title {
+
         padding: 0;
         margin-bottom: 0;
         font-size: 2em;
@@ -55,6 +56,9 @@
 
     .tbody-parameter-group {
         border-top: 0;
+        /* border-left: 1px solid #BCBCBC;
+            border-right: 1px solid #BCBCBC; */
+        /* border-bottom: 1px solid #BCBCBC; */
         padding: 0;
         margin: 0;
     }
@@ -73,9 +77,12 @@
         height: 99px;
         text-transform: uppercase;
         position: relative;
+        /* border-top: 1px solid #000;
+            border-bottom: 1px solid #000; */
         margin-bottom: 15px;
         background-color: rgb(240, 240, 240);
         background-image: url('https://forlearn.ispm.ao/instituicao-arquivo/{{ $institution->logotipo }}');
+        /*background-image: url('/img/CABECALHO_CINZA01GRANDE.png');*/
         background-position: 100%;
         background-repeat: no-repeat;
     }
@@ -115,13 +122,18 @@
         width: 85px;
         height: 100%;
         margin-bottom: 20px;
+
         background-position: 50%;
         margin-right: 8px;
     }
 
     .mediaClass td {
         border: 1px solid #fff;
+
+
     }
+
+
 
     .pl-1 {
         padding-left: 1rem !important;
@@ -145,15 +157,21 @@
     }
 
     .h1-title {
+
         padding: 0;
         margin-bottom: 0;
+        /* font-size: 14pt; */
         font-size: 1.40rem;
         padding-top: 40px;
+        /* background-color:red; */
+        /* font-weight: bold; */
         width: 100%;
+
     }
 
     .table_te {
         background-color: #F5F3F3;
+        ;
         width: 100%;
         text-align: right;
         font-family: calibri light;
@@ -169,7 +187,8 @@
     .table_te th {
         border-left: 1px solid #fff;
         border-bottom: 1px solid #fff;
-        padding: 4px !important;
+        padding: 4px;
+         !important;
         text-align: center;
         font-weight: bold;
     }
@@ -181,10 +200,14 @@
         font-size: 12pt;
     }
 
+    .tabble_te thead {}
+
     .bg0 {
         background-color: #2f5496 !important;
         color: white;
     }
+
+
 
     .bg1 {
         background-color: #8eaadb !important;
@@ -207,102 +230,93 @@
         height: 130px;
     }
 
-    .table-main {
+    .table-main{
         padding-left: 10px;
     }
 
-    .t-color {
-        color: #fc8a17;
-    }
-    
-    /* Apenas ajustes para alinhamento */
-    .pp1-center {
-        text-align: center;
-        font-size: 2.2em;
-        padding-top: 20px !important;
-        margin-bottom: 5px !important;
-    }
-    
-    .doc-name {
-        text-align: right;
-        padding-top: 5px !important;
-        padding-right: 7px;
-    }
-    
-    .doc-date {
-        text-align: right;
-        padding-right: 7px;
-        padding-top: 5px !important;
-        font-size: 0.9em;
-    }
-    
-    .logo-container {
-        width: 120px;
-        height: 68px;
-        background-size: contain !important;
-        background-repeat: no-repeat !important;
-        background-position: center center !important;
-    }
+    .t-color{
+            color:#fc8a17;
+        }
 </style>
-
 @php
-    $logotipo = 'https://' . $_SERVER['HTTP_HOST'] . '/instituicao-arquivo/' . $institution->logotipo;
-    $currentDate = \Carbon\Carbon::now()->format('d/m/Y');
+    $logotipo = 'https://' . $_SERVER['HTTP_HOST'] . '/instituicao-arquivo/' . $institution->logotipo; 
 @endphp
 
-<div class="div-top">
-    <table class="table table-main m-0 p-1 bg0" style="border:none; margin-top:-15px; margin-right:0; height:130px;">
-        <tr>
-            <!-- Coluna 1: Logo -->
-            <td class="td-fotografia logo-container" rowspan="3"
-                style="background-image:url('{{ $logotipo }}'); width:120px; height:68px; position:relative; top:10px; left:10px;">
-            </td>
-            
-            <!-- Coluna 2: Conteúdo principal -->
-            <td style="border:none; vertical-align:top;">
-                <!-- Linha 1: PP1 Centralizado -->
-                <div style="text-align:center; padding-top:15px;">
-                    <h1 class="h1-title pp1-center" style="color:#fc8a17; margin:0; padding:0;">
-                       <b>PP1</b>
-                    </h1>
-                </div>
-                
-                <!-- Linha 2: Nome do Documento -->
-                <div class="doc-name">
-                    <h1 class="h1-title" style="margin:0; padding:0;">
-                       <b>{{ $doc_name }}</b>
-                    </h1>
-                </div>
-                
-                <!-- Linha 3: Data de Geração -->
-                <div class="doc-date">
-                    <span style="color:white;">
-                        Documento gerado a
-                        <b>{{ $currentDate }}</b>
-                    </span>
-                </div>
-            </td>
-        </tr>
-    </table>
+<div class="div-top" style="">
+    <div class="div-top">
 
-    <!-- Nome da Instituição (mantido igual) -->
-    <div class="instituto" style="position: absolute; top: 8px; left: 130px; width: 450px; font-family: Impact; padding-top: 40px; color:white;">
-        <h4><b>
-            @if (isset($institution->nome))
-                @php
-                    $institutionName = mb_strtoupper($institution->nome, 'UTF-8');
-                    $new_name = explode(" ", $institutionName);
-                    foreach($new_name as $key => $value) {
-                        if($key == 1) {
-                            echo $value."<br>";
-                        } else {
-                            echo $value." "; 
-                        }
-                    }
-                @endphp
-            @else
-                Nome da instituição não encontrado
-            @endif 
-        </b></h4>
+
+        <table class="table table-main m-0 p-1 bg0 " style="border:none;margin-top:-15px!important;margin-right:0px!important">
+            <tr>
+
+                <td class="td-fotografia " rowspan="12"
+                    style="background-image:url('{{ $logotipo }}');width:120px; height:68px; background-size:90px 100px;
+                        background-repeat:no-repeat;Background-position:center center;
+                        border:none;padding-right:12px!important;top:10px;position:relative;left: 10px;
+                        ">
+                </td>
+
+            </tr>
+
+            <tr>
+                <td class="td-fotografia " rowspan="12" style=" width:200px; height:78px;border:none;"></td>
+
+            </tr>
+            <tr>
+                <td class="td-fotografia " rowspan="12" style=" width:200px; height:78px;border:none;"></td>
+            </tr>
+
+
+
+
+
+            <tr>
+                <td class="bg0" style="border:none; text-align:right;padding-right:7px;">
+                    <h1 class="h1-title" style="">
+                       <b> {{$doc_name}}</b>
+                    </h1>
+                </td>
+            </tr>
+            <tr>
+                <td class="data-generate" style="border:none; text-align:right;padding-right:7px;">
+
+                    <span class="" rowspan="1" style="padding-left:20%;position:relative;">
+                        Documento gerado a
+                        <b>{{ Carbon\Carbon::now()->format('d/m/Y') }}</b>
+                    </span>
+
+                </td>
+            </tr>
+
+        </table>
+
+
+
+
+
+        <div class="instituto"
+            style="position: absolute; top: 8px; left: 130px; width: 450px; font-family: Impact; padding-top: 40px;color:white;">
+            <h4><b>
+                @if (isset($institution->nome))
+                    @php
+                        $institutionName = mb_strtoupper($institution->nome, 'UTF-8');
+                        $new_name = explode(" ",$institutionName);
+                        foreach( $new_name as $key => $value ){
+                            if($key==1){
+                                echo $value."<br>";
+                            }else{
+                                echo $value." "; 
+                            }
+                        } echo "";
+                    @endphp
+                @else
+                    Nome da instituição não encontrado
+                @endif 
+                </b></h4>
+        </div>
+
     </div>
+
+
+
 </div>
