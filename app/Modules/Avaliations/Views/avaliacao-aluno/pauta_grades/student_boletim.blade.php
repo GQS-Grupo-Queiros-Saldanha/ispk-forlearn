ad0e11eb25fdbaad458dbc2c8a7a41f0b64648c5
EzequielFrancisco <ezequiel.p.a.francisco@gmail.com>
2026-01-30 10:46:30 +0100

    style(boletim): add comentario

--- app/Modules/Avaliations/Views/avaliacao-aluno/pauta_grades/student_boletim.blade.php
+++ app/Modules/Avaliations/Views/avaliacao-aluno/pauta_grades/student_boletim.blade.php
@@ -281,7 +281,7 @@
                             // Classificação MAC
                             var cor_media = '', classificacao = '-';
                             if (media !== null) {
-                                if (media >= 15.3) { classificacao='Aprovado(a)'; cor_media='for-green'; }
+                                if (media >= 15.3) { classificacao=''; cor_media='for-green'; }
                                 else if (media >= 7 && media < 15.3) { classificacao='Exame'; cor_media='for-yellow'; }
                                 else { classificacao='Recurso'; cor_media='for-red'; }
                             }
@@ -300,7 +300,7 @@
 
                             var estado_final = '-', cor_final = '';
                             if (media_final !== null) {
-                                if (media_final >= 15.3) { estado_final='Aprovado(a)'; cor_final='for-green'; }
+                                if (media_final >= 15.3) { estado_final=''; cor_final='for-green'; }
                                 else { estado_final='Reprovado(a)'; cor_final='for-red'; }
                             }
 
